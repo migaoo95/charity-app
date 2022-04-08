73ba6256814e8cbfcc3e42e66a58b1e4aca974cd
migaoo95 <45981784+migaoo95@users.noreply.github.com>
2022-04-08 16:51:03 +0100

    Responsive Navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,13 +6,19 @@ import { ReactComponent as List } from "../assets/svg/listIcon.svg";
 import { ReactComponent as Plus } from "../assets/svg/squarePlus.svg";
 import { ReactComponent as Exit } from "../assets/svg/exitIcon.svg";
 import { ReactComponent as Cart } from "../assets/svg/cartIcon.svg";
+import { HiMenu } from "react-icons/hi";
 import { GiLoveHowl } from "react-icons/gi";
-import { IoStorefront } from "react-icons/io";
+import { AiOutlineClose } from "react-icons/ai";
 import { useNavigate, Link } from "react-router-dom";
 // ------------------------- { google Auth }
 import { getAuth } from "firebase/auth";
+import { useState, useEffect } from "react";
 function Navbar() {
   const auth = getAuth();
+  const [menuToggle, setMenuToggle] = useState(true);
+  const handleMenuToggle = () => {
+    setMenuToggle((prev) => !prev);
+  };
   // const [changeDetails, setChangeDetails] = useState(false);
   // const [formData, setFormData] = useState({
   //   name: auth.currentUser.displayName,
@@ -27,28 +33,50 @@ function Navbar() {
   return (
     <header className={classes.header}>
       <div className={classes.header__main}>
+        <div className={classes.header__main__toggler}>
+          {menuToggle ? (
+            <HiMenu onClick={handleMenuToggle} size={35} />
+          ) : (
+            <AiOutlineClose onClick={handleMenuToggle} size={35} />
+          )}
+        </div>
         <div className={classes.header__main__logo}>
           <GiLoveHowl size={50} />
           <p>Charity.io</p>
         </div>
-        <nav className={classes.header__main__nav}>
+        <nav
+          className={`${classes.header__main__nav} ${
+            menuToggle ? classes.header__main__hide : ""
+          }`}
+        >
           <ul className="">
             <li className="">
-              <ShopIcon width={33} />
+              <div>
+                <ShopIcon width={33} />
+              </div>
               <span>Marketplace</span>
             </li>
             <li>
-              <ShieldHeart width={33} />
+              <div>
+                <ShieldHeart width={33} />
+              </div>
+
               <span>Charity</span>
             </li>
-            <li>
-              <List width={33} />
+            <li className={classes.header__main__nav__active}>
+              <div>
+                <List width={33} />
+              </div>
+
               <span>
-                <Link to="/listing">Your Listings</Link>
+                <Link to="/listing">My Listings</Link>
               </span>
             </li>
             <li>
-              <Plus width={33} />
+              <div>
+                <Plus width={33} />
+              </div>
+
               <span>Create Listing</span>
             </li>
           </ul>
