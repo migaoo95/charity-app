e7900881da3a45dfb3c04db64d6ca4106600facc
migaoo95 <45981784+migaoo95@users.noreply.github.com>
2022-07-12 15:18:09 +0100

    Hosting / Node furebase functions

--- client/src/components/Checkout.jsx
+++ client/src/components/Checkout.jsx
@@ -1,27 +1,29 @@
 import { useEffect, useState } from "react";
-import classes from '../styles/modules/Cart/Checkout.module.scss'
-function Checkout({disabled, items, shipping}) {
+import classes from "../styles/modules/Cart/Checkout.module.scss";
+function Checkout({ disabled, items, shipping }) {
   const [backendData, setBackendData] = useState([{}]);
-  const [checkoutItems, setCheckoutItems] = useState([{}])
-  const [shippingData, setShippingData] = useState([{typeName: 'Standard', cost: 5, duration: [3,5]},{typeName: 'Express', cost: 9, duration: [1,3]}])
+  const [checkoutItems, setCheckoutItems] = useState([{}]);
+  const [shippingData, setShippingData] = useState([
+    { typeName: "Standard", cost: 5, duration: [3, 5] },
+    { typeName: "Express", cost: 9, duration: [1, 3] },
+  ]);
   useEffect(() => {
     let itemsStripedStructure = [];
-if(items){
-  items.forEach(item =>{
-    item = {
-      priceInCents: item.price * 100,
-      name: item.name,
-      quantity: 1,
-      images: [item.imageUrls[0]],
-      currency: 'usd'
+    if (items) {
+      items.forEach((item) => {
+        item = {
+          priceInCents: item.price * 100,
+          name: item.name,
+          quantity: 1,
+          images: [item.imageUrls[0]],
+          currency: "usd",
+        };
+        itemsStripedStructure.push(item);
+      });
+      setCheckoutItems(itemsStripedStructure);
+      //  console.log(items, 'Oryginal');
+      //  console.log(itemsStripedStructure, 'Stripped');
     }
-    itemsStripedStructure.push(item);
-   
-   })
-   setCheckoutItems(itemsStripedStructure)
-  //  console.log(items, 'Oryginal');
-  //  console.log(itemsStripedStructure, 'Stripped');
-}
 
     // fetch("/api")
     //   .then((res) => {
@@ -39,13 +41,11 @@ if(items){
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        // TODO: Push items in here 
-        items: 
-          checkoutItems,
-         shipping: shipping ? shippingData[0] : shippingData[1]
-          // { priceInCents:10000, name:"learn React", quantity: 1 },
-          // { priceInCents:10000, name:"learn React", quantity: 1 },
-        
+        // TODO: Push items in here
+        items: checkoutItems,
+        shipping: shipping ? shippingData[0] : shippingData[1],
+        // { priceInCents:10000, name:"learn React", quantity: 1 },
+        // { priceInCents:10000, name:"learn React", quantity: 1 },
       }),
     })
       .then((res) => {
@@ -63,7 +63,11 @@ if(items){
   };
   return (
     <div className={classes.buttonContainer}>
-      <button disabled={disabled}  className={ `${disabled && classes.buttonContainer__disabled}`} onClick={handleCheckout}>
+      <button
+        disabled={disabled}
+        className={`${disabled && classes.buttonContainer__disabled}`}
+        onClick={handleCheckout}
+      >
         Checkouttt
       </button>
     </div>
--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
-require('dotenv').config()
+require("dotenv").config();
 // Import express
 const express = require("express");
 const app = express();
 app.use(express.json());
-app.use(express.static('client'));
+// app.use(express.static(`${_dirname}/build`));
+app.use(express.static(path.join(__dirname, "/build")));
 const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
 // console.log(process.env.STRIPE_PRIVATE_KEY)
-if(process.env.SERVER_URL) { 
-    console.log('It is set!'); 
-}
-else { 
-    console.log('No set!'); 
+if (process.env.SERVER_URL) {
+  console.log("It is set!");
+} else {
+  console.log("No set!");
 }
 // Route for an api --- backend apiss
 // app.post("/api", async (req, res) => {
@@ -24,72 +24,62 @@ else {
 //   [1, { priceInCents: 10000, name: "Learn React" }],
 //   [2, { priceInCents: 20000, name: "Learn Css" }],
 // ]);
-const item = [{priceInCents:10000, name:"learn React", quantity: 1} ]
-console.log(item, 'item')
+const item = [{ priceInCents: 10000, name: "learn React", quantity: 1 }];
+console.log(item, "item");
 // 1. Uncomment
-app.post('/api', async (req,res)=>{
-    try {
-        console.log(req.body)
-        const session = await stripe.checkout.sessions.create({
-            payment_method_types: ["card"],
-            mode: "payment",
-            shipping_options: [ {
-                shipping_rate_data: {
-                  type: 'fixed_amount',
-                  fixed_amount: {
-                    amount: req.body.shipping.cost * 100,
-                    currency: 'gbp',
-                  },
-                  display_name: req.body.shipping.typeName,
-                  // Delivers between 5-7 business days
-                  delivery_estimate: {
-                    minimum: {
-                      unit: 'business_day',
-                      value: req.body.shipping.duration[0],
-                    },
-                    maximum: {
-                      unit: 'business_day',
-                      value: req.body.shipping.duration[1],
-                    },
-                  }
-                }
-              },],
-            line_items: req.body.items.map(item=>{
-                return {
-                    price_data:{
-                        currency: "gbp",
-                        product_data: {
-                            name: item.name,
-                            images: item.images
-                        },
-                        unit_amount: item.priceInCents
-                    }, quantity:item.quantity
-                }
-            }),
-        //     // line_items: req.body.items.map(item => {
-        //     //   const storeItem = storeItems.get(item.id)
-        //     //   return {
-        //     //     price_data: {
-        //     //       currency: "gbp",
-        //     //       product_data: {
-        //     //         name: storeItem.name,
-        //     //       },
-        //     //       unit_amount: storeItem.priceInCents,
-        //     //     },
-        //     //     quantity: item.quantity,
-        //     //   }
-        //     // }),
-            success_url: `${process.env.SERVER_URL}/success`,
-            cancel_url: `${process.env.SERVER_URL}/cart`,
-          })
-          res.json({ url: session.url })
-    } catch (err){
-        res.status(500).json({ error: err.message})
-    }
-
-})
+app.post("/api", async (req, res) => {
+  try {
+    console.log(req.body);
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      shipping_options: [
+        {
+          shipping_rate_data: {
+            type: "fixed_amount",
+            fixed_amount: {
+              amount: req.body.shipping.cost * 100,
+              currency: "gbp",
+            },
+            display_name: req.body.shipping.typeName,
+            // Delivers between 5-7 business days
+            delivery_estimate: {
+              minimum: {
+                unit: "business_day",
+                value: req.body.shipping.duration[0],
+              },
+              maximum: {
+                unit: "business_day",
+                value: req.body.shipping.duration[1],
+              },
+            },
+          },
+        },
+      ],
+      line_items: req.body.items.map((item) => {
+        return {
+          price_data: {
+            currency: "gbp",
+            product_data: {
+              name: item.name,
+              images: item.images,
+            },
+            unit_amount: item.priceInCents,
+          },
+          quantity: item.quantity,
+        };
+      }),
+      success_url: `${process.env.SERVER_URL}/success`,
+      cancel_url: `${process.env.SERVER_URL}/cart`,
+    });
+    // res.json({ url: session.url });
+    res.json({ url: session.url });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 app.listen(5000, () => console.log("server started on port 5000"));
- /// -----------------------------------------------
+/// -----------------------------------------------
 //  require("dotenv").config();
 
 // const express = require("express");
